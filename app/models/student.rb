80250c36571d7780b489656db1e1f25d3e65b9df
ogola5 <ogolaevance5@gmail.com>
2022-11-20 06:28:31 +0300

    done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ApplicationRecord
+  def self.by_name(name)
+    Student.where("first_name LIKE ? OR last_name LIKE ?","%#{name}","%#{name}")
+  end
 
   def to_s
     "#{first_name} #{last_name}"
